d8eb131cdc4776a41d78760b3e8c7c5164373fd5
Lance Jasper Quitalig <laquitalig@up.edu.ph>
2022-10-07 07:50:09 +0000

    Parsing code into separate chunks to avoid clutter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import Fastify from 'fastify';
-import { getDB, saveDB } from './utils/db/index.js';
-import { v4 } from 'uuid';
+import { general } from './services/general/index.js';
+import { createToDo } from './services/todos/create-todo.js';
 
 const prefix = '/api';
 
@@ -8,35 +8,10 @@ export async function build () {
   // Initialize fastify
   const fastify = Fastify({ logger: true });
 
-  fastify.get(prefix, async (request, reply) => {
-    return { success: true };
-  });
+  fastify.get(prefix, general);
 
   // Create ToDo
-  fastify.post(`${prefix}/todo`, async (request, reply) => {
-    const { body } = request;
-    const { title, description, isDone = false } = body;
-    const db = await getDB();
-
-    const id = v4();
-
-    const todo = {
-      title,
-      description,
-      isDone,
-      createdDate: new Date().getTime(),
-      updatedDate: new Date().getTime()
-    };
-
-    db.todos[id] = todo;
-
-    await saveDB(db);
-
-    return {
-      id,
-      ...todo
-    };
-  });
+  fastify.post(`${prefix}/todo`, createToDo);
 
   return fastify;
 }
