e1c7d5e518a3646d2f2fa03f849cd8b6a10f483b
Lance Jasper Quitalig <laquitalig@up.edu.ph>
2022-10-07 05:03:25 +0000

    Moved fastify to app.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
-import Fastify from 'fastify';
+import { build } from './app.js';
 
 async function start () {
-    try {
-        // Initialize fastify
-        const fastify = Fastify({ logger: true });
+  try {
+    const fastify = await build();
 
-        fastify.get('/api', async (request, reply) => {
-            return { success: true };
-        });
-
-        const addr = await fastify.listen({
-            port: '8080'
-        });
-
-        console.log('Listening on %s', addr);
-        
-    } catch (error) {
-        // Error printing
-        console.error(error);
-        // Exits program with error code
-        process.exit(1);
-    }
-};
+    const addr = await fastify.listen({
+      port: '8080'
+    });
+    console.log('Listening on %s', addr);
+  } catch (error) {
+    // Error printing
+    console.error(error);
+    // Exits program with error code
+    process.exit(1);
+  }
+}
 
 start();
